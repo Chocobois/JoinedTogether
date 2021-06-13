04f9609ba990411b64488379b6886b2c93c92341
ArcticFqx <dan-eric.naess@live.no>
2021-06-13 02:47:11 +0200

    Try this golen

--- src/scripts/components/World.ts
+++ src/scripts/components/World.ts
@@ -2,37 +2,36 @@ import { GameScene } from "../scenes/GameScene";
 
 export class World extends Phaser.GameObjects.Container {
 	public scene: GameScene;
-  public properties: {}
+  public properties: {};
 
   private world: Phaser.Tilemaps.Tilemap;
 
+  private loadProps()
+  {
+    this.properties = {};
+    for( let index in this.world.properties )
+    {
+      let prop = this.world.properties[index];
+      this.properties[prop.name] = prop.value;
+    }
+  }
   constructor(scene: GameScene, x: number, y: number) {
 		super(scene, x, y);
 		this.scene = scene;
 		this.scene.add.existing(this);
-
     this.world = scene.make.tilemap({ key: 'map' });
+    this.loadProps();
 
-    let properties = {};
-    for( let index in this.world.properties )
-    {
-      let prop = this.world.properties[index];
-      properties[prop.name] = prop.value;
-    }
 
     let tileset: Phaser.Tilemaps.Tileset[] = [];
     for( let tile of this.world.tilesets )
       tileset.push(this.world.addTilesetImage(tile.name, tile.name));
 
-    let scale = properties["Scale"];
+    let scale = this.properties["Scale"];
 
     let layer = this.world.createLayer('Background', tileset);
     layer.setPosition(layer.x*scale,layer.y*scale);
     layer.setScale(scale);
 
-    /*
-    let layer2 = this.world.createLayer('Foreground', tileset);
-    layer2.setX(100);
-    */
   }
 }
